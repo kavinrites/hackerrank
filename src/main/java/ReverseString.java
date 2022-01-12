8d32edb920ab341ea2c9fda9f4117458cd653bd6
tekipanda <t3k1panda@gmail.com>
2022-01-11 22:56:48 -0700

    simple factorial using recursion reversing string using recursion

---
+++ src/main/java/ReverseString.java
@@ -0,0 +1,14 @@
+public class ReverseString {
+    public static void main(String[] args) {
+        System.out.println(revString("kavi", 4));
+        System.out.println(revString("Saranya", "Saranya".length()));
+    }
+
+    public static String revString(String ip, int len) {
+        if(len == 1){
+            return ip;
+        } else {
+            return ip.substring(len-1) + revString(ip.substring(0,len-1),len-1);
+        }
+    }
+}
