35fbf195b13e8436117c180f35cc252093800c6f
Kavinaesh Dheenadayalan <Kavinaesh.Dheenadayalan@aexp.com>
2024-09-10 20:32:33 -0700

    divisible by 3

--- src/main/java/DivisibleBy3.java
+++ src/main/java/DivisibleBy3.java
@@ -1,5 +1,28 @@
 public class DivisibleBy3 {
   public static void main(String[] args) {
-    System.out.println("Numbers divisible by 3 are:");
+    System.out.println(isDivisibleBy3(-3039));
+  }
+  
+  public static boolean isDivisibleBy3(int ip) {
+    String ipStr = String.valueOf(Math.abs(ip));
+    int n = 0;
+    for(int i = 0; i<ipStr.length(); i++){
+        n += Integer.valueOf(ipStr.substring(i,i+1));
+    }
+    System.out.println("n:" + n);
+    if(n > 0 ) {
+        while (n > 2) {
+            n = n - 3;
+        }
+    } else {
+        while (n < -2) {
+            n = n + 3;
+        }
+    }
+    System.out.println("n:" + n);
+    if (n == 0) {
+      return true;
+    }
+    return false;
   }
 }
